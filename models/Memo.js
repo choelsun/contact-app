bff78e85dc03ca32b6e6e3b97553372e0d33d668
kim-chiel-sum <140506s@gmail.com>
2024-08-30 18:05:51 +0900

    Memo 모델 구현, 관계설정

--- models/Memo.js
+++ models/Memo.js
@@ -1,32 +1,20 @@
 // const { Sequelize, DataTypes, Model } = require('sequelize');
-import { Sequelize, DataTypes, Model, ForeignKeyConstraintError } from "sequelize";
-// import { FOREIGNKEYS } from "sequelize/lib/query-types";
-// import { FOREIGNKEYS } from "sequelize/lib/query-types";
+import { Sequelize, DataTypes, Model } from "sequelize";
 // Sequelize의 defaultValue는 NULL
 
-class User extends Model {
+class Memo extends Model {
     static init(sequelize) {
         return super.init(
             {
                 // Model attributes are defined here : 시퀄라이즈는 기본적으로 ID컬럼을 생성하고 PK로 정의함
                 name: {
-                    type: DataTypes.STRING(30),
+                    type: DataTypes.STRING(100),
                     allowNull: false,
                     
                 },
-                phone: {
-                    type: DataTypes.STRING(11),
-                    allowNull: false,
-                    unique: true
-                },
-                email: {
-                    type: DataTypes.STRING(100),
-                    // allowNull: is ture: NOT NULL
-                    unique: true
-                },
-                relationship: {
-                    type: DataTypes.ENUM('Family', 'friend', 'others'),
-                    // defaultValue: null
+                user_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
                 },
                 created_at: {
                     type: DataTypes.DATE,
@@ -39,8 +27,8 @@ class User extends Model {
                 sequelize, // DB 연결을 위한 시퀄라이즈 인스턴스
                 timestamps: false,
                 underscored: true,
-                modelName: 'User', // 시퀄라이즈 모델 이름
-                tableName: 'users',// 데이터베이스 테이블 이름 (기본 값: 모델 이름의 복수형)
+                modelName: 'Memo', // 시퀄라이즈 모델 이름
+                tableName: 'Memos',// 데이터베이스 테이블 이름 (기본 값: 모델 이름의 복수형)
                 paranoid: false,   // 테이블 삭제 복구를 원하면 true, 아니면 false (삭제 날짜를 기록)
                 charset: "utf8mb4",// 이모지 삽입을 위해서는 ut8mb4 형식
                 collate: "utf8mb4_general_ci"
@@ -48,12 +36,12 @@ class User extends Model {
         );
     }
     static associate(db) {
-        User.hasMany(db.Memo, {foreignKey: "user_id", sourceKey: "id"});
+        Memo.belongsTo(db.User, {foreignKey: "user_id", targetKey: "id"});
         // db.User.hasMany (dv.User.Memo, {ForeignKey: "user_id", so})
     } 
 }
 
 
 
-// 모델 외부에서 User 정보를 생성할 때 참조
-export default User;
+// 모델 외부에서 Memo 정보를 생성할 때 참조
+export default Memo;
--- models/index.js
+++ models/index.js
@@ -6,9 +6,11 @@ dotenv.config;
 import config from "../config/config.json" assert {type: 'json'};
 const env = process.env.NODE_ENV || "development";
 const configEnv = config[env]; // username, database, password, host...
-console.log(env);
+// console.log(env);
 // import User from "./User.js"; // User 모델 불러오기
 
+import User from "./User.js";
+import Memo from "./Memo.js";
 
 import { Sequelize } from "sequelize";
 // Option 3: Passing parameters separately (other dialects)
@@ -18,28 +20,34 @@ const sequelize = new Sequelize(configEnv.database, configEnv.username, configEn
   port: 3306,
   logging: (msg) => console.log(msg)
 });
-// DB 연결 테스트
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+User.init(sequelize); // User 모델의 컬럼, 자료형 --> mariaDB에 생성
+Memo.init(sequelize); 
 
-await sequelize.sync({ force: true }).then(() => {
-  console.log('All models were synchronized successfully.');
+// // DB 연결 테스트
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
+
+await sequelize.sync({ force : false }).then(() => {
+  console.log('======== 모델과 데이터베이스가 맵핑 되었습니다 =========');
 }).catch((error) => {
-  console.log('Error Occured' + error)
+  console.log('======== 에러내용 ========' + error)
 });
 
 
 const db = {}; // 여러 객체(모델)를 한번에 저장해서 db라는 이름으로 내보내기
-import User from "./User.js";
+
 
 //객체, 속성= 값
 db.sequelize = sequelize; // 시퀄라이즈, 인스턴스(db접속정보)
 db.User = User;
-User.init(sequelize); // User 모델의 컬럼, 자료형 --> mariaDB에 생성
+db.Memo = Memo;
 // Model - Table 동기화(Synchronization) ==> Promises 객체를 반환 : 비동기
+
+User.associate(db);
+Memo.associate(db);
 
 export default db;
