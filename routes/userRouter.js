3e6f887fc89e95f120b4313c8e2285e54fd5af43
kim-chiel-sum <140506s@gmail.com>
2024-08-30 18:05:28 +0900

    uesr 라우터 완료

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,7 +11,15 @@ const timeLog = (req, res, next) => {
 router.use(timeLog)
 
 // User(연락처) 생성
-router.post('/', userControl.createUser);
+router.get('/user/:id', userControl.findOneUser); // 동적 파라미터
+router.post('/user', userControl.createUser);
+// Users(연락처) 조회
+router.get('/users', userControl.findAllUsers);
+
+router.delete('/users', userControl.removeAllUsers);
+router.delete('/user/:id', userControl.removeUser);
+// User연락처 수정
+router.put('/user', userControl.updateUser);
 
 // module.exports = router
 export default router;
